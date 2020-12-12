8e7b2c529bd375f2ad27f7ee0b39d078398bc764
shreeniv <69985129+shreeniv@users.noreply.github.com>
2020-12-12 14:16:03 -0500

    checking in data extracted from json into the webpage

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -23,3 +23,38 @@ tableData.forEach(function(ufoSighting) {
     cell.text(value);
   });
 });
+
+// Select the filter button and create function for click
+var button = d3.select("#filter-btn");
+button.on("click", function() {
+
+    tbody.html("");
+
+    // Select the input date get the raw HTML nodes
+    var inputElement = d3.select("#datetime");
+    // Get the value property of the input date, state, shape
+    var inputValue = inputElement.property("value");
+    // console.log of input value
+    console.log(inputValue);
+    // Filter Data with datetime equal to input value
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+    // console.log of filter values
+    console.log(filteredData);
+
+    // Once we have our output run through the selections
+    filteredData.forEach(function(selections) {
+
+    console.log(selections);
+    
+    // Append one table row `tr` for each UFO Sighting object
+    var row = tbody.append("tr");
+    
+    // Use `Object.entries` to console.log each UFO Sighting value
+    Object.entries(selections).forEach(function([key, value]) {
+        console.log(key, value);
+        // Append a cell to the row for each value
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+});
